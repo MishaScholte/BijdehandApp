bfd1b3ee9a38e09869c92eec17f3dd5c7f5d740e
Misha Scholte <mishascholte@gmail.com>
2026-02-09 11:55:14 +0100

    style: update voucher card background to sexy orange gradient

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
               description="Voorkom dat je geld verliest en ontvang een melding voordat de bon verloopt"
               icon={Ticket}
               iconColor="text-white"
-              iconBgClass="bg-gradient-to-br from-red-500 to-red-700 border border-white/20"
+              iconBgClass="bg-gradient-to-br from-orange-400 to-orange-600 border border-white/20"
               iconClassName="w-16 h-16"
               className="md:col-span-2"
             />
